33574eb4b25ae51140e07523688912238fba80b3
Marc Schwering <schweringmarc01@gmail.com>
2018-01-21 17:04:24 +0100

    scraping Fundamentalanalysen 1ster Versuch

--- finData/load.py
+++ finData/load.py
@@ -63,6 +63,84 @@ def scrapeWikiTable(url_chr, class_chr="wikitable sortable"):
     return {"head": header, "body": body}
 
 
+# testStock = {
+#     'Name': 'Addidas AG',
+#     'Typ': 'Aktie',
+#     'WKN': 'A1EWWW',
+#     'ISIN': 'DE000A1EWWW0',
+#     'yahooTicker': 'ADS.DE',
+#     'boerseURL': 'Adidas-Aktie/DE000A1EWWW0'
+# }
+# boerseDe = {
+#     'host': 'www.boerse.de',
+#     'pathFund': 'fundamental-analyse'
+# }
+
+#url_chr = 'https://%s/%s/%s' % (boerseDe['host'], boerseDe['pathFund'], testStock['boerseURL'])
+
+
+def htmlTab2dict(tab, hasRownames=True, hasColnames=True, removeEmpty=True):
+    rows = tab.findAll('tr')
+    rownamesIdx = 0 if hasRownames else -1
+    colnamesIdx = 0 if hasColnames else -1
+
+    data = []
+    nonEmptyRowIdxs = []
+    for idx, row in enumerate(rows[(colnamesIdx+1):]):
+        thisRow = []
+        for col in row.findAll('td')[(rownamesIdx+1):]:
+            text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
+            thisRow.append(text)
+        if not removeEmpty or not all(d == '' for d in thisRow):
+            data.append(thisRow)
+            nonEmptyRowIdxs.append(idx + (colnamesIdx+1))
+    out = {'data': data}
+
+    if hasRownames:
+        rownames = []
+        for row in [rows[i] for i in nonEmptyRowIdxs]:
+            text = row.find('td').text.replace(u'\xa0', u' ').encode('utf-8').strip()
+            rownames.append(text)
+        out['rownames'] = rownames
+
+    if hasColnames:
+        colnames = []
+        for col in rows[colnamesIdx].findAll('td')[1:]:
+            text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
+            colnames.append(text)
+        out['colnames'] = colnames
+
+    if len(out['data']) > 1:
+        lens = [len(row) for row in out['data']]
+        diffs = map(lambda x: x[0] - x[1], zip(lens, lens[1:] + [lens[0]]))
+        if not all(d == 0 for d in diffs):
+            raise UserWarning('Data rows do not all have the same lengths')
+
+    return out
+
+
+
+
+def FundamentalTables(url_chr,
+                   ids = ['guv', 'bilanz', 'kennzahlen', 'rentabilitaet', 'personal'],
+                   texts = ['Marktkapitalisierung']):
+    tabDict = {}
+    soup = BeautifulSoup(urllib2.urlopen(url_chr), "lxml")
+    for id in ids:
+        h3 = soup.find(lambda tag: tag.get('id') == id and tag.name == 'h3')
+        tabDict[id.lower()[:6]] = h3.findNext('table')
+    for text in texts:
+        h3 = soup.find(lambda tag: text in tag.text and tag.name == 'h3')
+        tabDict[text.lower()[:6]] = h3.findNext('table')
+    return tabDict
+    out = {}
+    for key, tab in tabDict.iteritems():
+        out[key] = htmlTab2dict(tab, hasRownames=True, hasColnames=True, removeEmpty=True)
+    return out
+
+
+
+
 
 def main():
     dax = "https://en.wikipedia.org/wiki/DAX"
--- test/test_load.py
+++ test/test_load.py
@@ -4,17 +4,26 @@ import finData.load
 class load_finData(unittest.TestCase):
 
     def setUp(self):
-        self.ticker = ["ADS"]
-        self.url = "https://en.wikipedia.org/wiki/DAX"
+        pass
 
     def tearDown(self):
-        self.ticker = None
+        pass
 
-    def test_getHistoricalPrices(self):
-        self.histPrices = finData.load.getHistoricPrices(self.ticker)
-
-    def test_scrapeWikiTable(self):
-        self.table = finData.load.scrapeWikiTable(self.url)
+    def test_htmlTab2dict(self):
+        htmlTable = (u'<table><tbody>' +
+                u'<tr><td>&nbsp;</td><td>2008</td><td>2009</td><td>2010</td><td>2011</td><td>2012</td></tr>' +
+                u'<tr><td>Personal am Jahresende</td><td>38.982</td><td>39.596</td><td>42.541</td><td>46.824</td><td>46.306</td></tr>' +
+                u'<tr><td>Gewinn je Mitarbeiter</td><td>16.469,14</td><td>6.187,49</td><td>13.328,32</td><td>13.091,58</td><td>11.359,22</td></tr>' +
+                u'<tr><td colspan="10"><div><h3>Personal</h3><div>blablabla</div></div></td></tr>' +
+                u'</tbody></table>')
+        res = finData.load.htmlTab2dict(BeautifulSoup(htmlTable))
+        self.assertEqual(res['colnames'], ['2008', '2009', '2010', '2011', '2012'])
+        self.assertEqual(res['rownames'], ['Personal am Jahresende', 'Gewinn je Mitarbeiter'])
+        self.assertEqual(res['data'][0], ['38.982', '39.596', '42.541', '46.824', '46.306'])
+        res = finData.load.htmlTab2dict(BeautifulSoup(htmlTable), hasRownames=False, removeEmpty=False)
+        self.assertEqual(res['data'][2], ['Personalblablabla'])
+        htmlTable2 = htmlTable[:150] + htmlTable[170:]
+        self.assertRaises(UserWarning, findData.load.htmlTab2dict(BeautifulSoup(htmlTable2)))
 
 if __name__ == '__main__':
     unittest.main()
