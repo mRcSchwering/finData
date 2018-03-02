220491ff3730b3bd507bd5c61a75f4702b3a9aee
Marc Schwering <schweringmarc01@gmail.com>
2018-03-02 23:21:29 +0100

    Updates
    zu python 3 gewechselt
    OOP -> Klasse gebaut

--- finData/load.py
+++ finData/load.py
@@ -2,103 +2,129 @@
 
 Detailed Description
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 #import pandas_datareader as pdr   # deprecated prd.get_data_yahoo ???
 #import pandas_datareader.data as web # usable DataReader ???
 #from tiingo import TiingoClient # useful??: No!
 
 import datetime
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 
+class Loader(object):
+    host = 'www.boerse.de'
+    fund_route = 'fundamental-analyse'
+    divid_route = 'dividenden'
 
 
-def htmlTab2dict(tab,
-                 hasRownames = True,
-                 hasColnames = True,
-                 removeEmpty = True,
-                 checkTable = True):
-    """Return dict of lists from html table string"""
-    rows = tab.findAll('tr')
-    rownamesIdx = 0 if hasRownames else -1
-    colnamesIdx = 0 if hasColnames else -1
-
-    data = []
-    nonEmptyRowIdxs = []
-    for idx, row in enumerate(rows[(colnamesIdx+1):]):
-        thisRow = []
-        for col in row.findAll('td')[(rownamesIdx+1):]:
-            text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
-            thisRow.append(text)
-        if not removeEmpty or not all(d == '' for d in thisRow):
-            data.append(thisRow)
-            nonEmptyRowIdxs.append(idx + (colnamesIdx+1))
-    out = {'data': data}
-
-    if hasRownames:
-        rownames = []
-        for row in [rows[i] for i in nonEmptyRowIdxs]:
-            text = row.find('td').text.replace(u'\xa0', u' ').encode('utf-8').strip()
-            rownames.append(text)
-        out['rownames'] = rownames
-
-    if hasColnames:
-        colnames = []
-        tag = 'th' if len(rows[colnamesIdx].findAll('th')) > 0 else 'td'
-        for col in rows[colnamesIdx].findAll(tag)[(rownamesIdx+1):]:
-            text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
-            colnames.append(text)
-        out['colnames'] = colnames
-
-    if checkTable and len(out['data']) > 1:
-        lens = [len(row) for row in out['data']]
-        diffs = map(lambda x: x[0] - x[1], zip(lens, lens[1:] + [lens[0]]))
-        if not all(d == 0 for d in diffs):
-            raise UserWarning('Data rows do not all have the same lengths')
-
-    return out
+    def __init__(self, name, typ, wkn, isin, boerse_name):
+        self.name = str(name)
+        self.typ = str(typ)
+        self.wkn = str(wkn)
+        self.isin = str(isin)
+        self.boerse_name = str(boerse_name)
+        self.__resolve_boerse_urls()
 
 
+    def __resolve_boerse_urls(self):
+        pre = 'https://' + Loader.host
+        post = self.boerse_name + '/' + self.isin
+        self.fund_url = '/'.join([pre, Loader.fund_route, post])
+        self.divid_url = '/'.join([pre, Loader.divid_route, post])
 
 
-def FundamentalTables(url_chr,
-                      ids = ['guv', 'bilanz', 'kennzahlen', 'rentabilitaet', 'personal'],
-                      texts = ['Marktkapitalisierung']):
-    """Scrape fundamental data tables from boerse.de given h3 Ids or h3 text search strings"""
-    tabDict = {}
-    soup = BeautifulSoup(urllib2.urlopen(url_chr), 'lxml')
-    for id in ids:
-        h3 = soup.find(lambda tag: tag.get('id') == id and tag.name == 'h3')
-        try:
-            tabDict[id.lower()[:6]] = h3.findNext('table')
-        except AttributeError:
-            print('Table %s was not found' % id)
-    for text in texts:
+    def __htmlTab2dict(self, tab,
+                       hasRownames = True,
+                       hasColnames = True,
+                       removeEmpty = True,
+                       checkTable = True):
+        """Return dict of lists from html table string"""
+        rows = tab.findAll('tr')
+        rownamesIdx = 0 if hasRownames else -1
+        colnamesIdx = 0 if hasColnames else -1
+
+        data = []
+        nonEmptyRowIdxs = []
+        for idx, row in enumerate(rows[(colnamesIdx+1):]):
+            thisRow = []
+            for col in row.findAll('td')[(rownamesIdx+1):]:
+                text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
+                thisRow.append(text)
+            if not removeEmpty or not all(d == '' for d in thisRow):
+                data.append(thisRow)
+                nonEmptyRowIdxs.append(idx + (colnamesIdx+1))
+        out = {'data': data}
+
+        if hasRownames:
+            rownames = []
+            for row in [rows[i] for i in nonEmptyRowIdxs]:
+                text = row.find('td').text.replace(u'\xa0', u' ').encode('utf-8').strip()
+                rownames.append(text)
+            out['rownames'] = rownames
+
+        if hasColnames:
+            colnames = []
+            tag = 'th' if len(rows[colnamesIdx].findAll('th')) > 0 else 'td'
+            for col in rows[colnamesIdx].findAll(tag)[(rownamesIdx+1):]:
+                text = col.text.replace(u'\xa0', u' ').encode('utf-8').strip()
+                colnames.append(text)
+            out['colnames'] = colnames
+
+        if checkTable and len(out['data']) > 1:
+            lens = [len(row) for row in out['data']]
+            diffs = map(lambda x: x[0] - x[1], zip(lens, lens[1:] + [lens[0]]))
+            if not all(d == 0 for d in diffs):
+                raise UserWarning('Data rows do not all have the same lengths')
+        return out
+
+
+    def getFundamentalTables(self, returnTable = False,
+                             ids = ['guv', 'bilanz', 'kennzahlen', 'rentabilitaet', 'personal'],
+                             texts = ['Marktkapitalisierung']):
+        """Scrape fundamental data tables from boerse.de given h3 Ids or h3 text search strings"""
+        tabDict = {}
+        soup = BeautifulSoup(requests.get(self.fund_url).content, 'lxml')
+        for id in ids:
+            h3 = soup.find(lambda tag: tag.get('id') == id and tag.name == 'h3')
+            try:
+                tabDict[id.lower()[:6]] = h3.findNext('table')
+            except AttributeError:
+                print('Table %s was not found' % id)
+        for text in texts:
+            h3 = soup.find(lambda tag: text in tag.text and tag.name == 'h3')
+            try:
+                tabDict[text.lower()[:6]] = h3.findNext('table')
+            except AttributeError:
+                print('Table %s was not found' % text)
+        out = {}
+        for key, tab in tabDict.items():
+            out[key] = self.__htmlTab2dict(tab, hasRownames=True, hasColnames=True, removeEmpty=True)
+        self.fund_table = out
+        if returnTable:
+            return out
+
+
+    def getDividendTable(self, returnTable = False,
+                         text = 'Dividenden'):
+        """Scrape dividend table from boerse.de given a h3 text search string"""
+        soup = BeautifulSoup(requests.get(self.divid_url).content, 'lxml')
         h3 = soup.find(lambda tag: text in tag.text and tag.name == 'h3')
         try:
-            tabDict[text.lower()[:6]] = h3.findNext('table')
+            tab = h3.findNext('table')
         except AttributeError:
-            print('Table %s was not found' % text)
-
-    out = {}
-    for key, tab in tabDict.iteritems():
-        out[key] = htmlTab2dict(tab, hasRownames=True, hasColnames=True, removeEmpty=True)
-    return out
+            print('Table %s not found' % text)
+        self.divid_table = self.__htmlTab2dict(tab, hasRownames=False)
+        if returnTable:
+            return self.divid_table
 
 
 
 
-def DividendTable(url_chr, text = 'Dividenden'):
-    soup = BeautifulSoup(urllib2.urlopen(url_chr), 'lxml')
-    h3 = soup.find(lambda tag: text in tag.text and tag.name == 'h3')
-    try:
-        tab = h3.findNext('table')
-    except AttributeError:
-        print('Table %s not found' % text)
-    return htmlTab2dict(tab, hasRownames=False)
+
+
+
+
+
 
 
 
@@ -106,12 +132,12 @@ def DividendTable(url_chr, text = 'Dividenden'):
 
 # TODO eigene API bauen!!
 
-ticker = "ADS"
-start = datetime.datetime(1990, 1, 1)
-
-def HistoricPrices(ticker,
-                   start = datetime.datetime(1990, 1, 1)):
-    """Download historic prices from yahoo! using ticker symbol"""
+# ticker = "ADS"
+# start = datetime.datetime(1990, 1, 1)
+#
+# def HistoricPrices(ticker,
+#                    start = datetime.datetime(1990, 1, 1)):
+#     """Download historic prices from yahoo! using ticker symbol"""
     # koennte man auch scrapen (aber dann ohne adjusted Prices)
     # Bsp: https://www.boerse.de/historische-kurse/BB-Biotech-Aktie/CH0038389992_seite,231,anzahl,20
     #return pdr.get_data_yahoo(ticker, start=start)
--- test/test_load.py
+++ test/test_load.py
@@ -1,50 +1,41 @@
 import unittest
 import finData.load
 
+
 class scraping_boerse(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.a = finData.load.Loader('Addidas AG', 'Aktie', 'A1EWWW', 'DE000A1EWWW0', 'Adidas-Aktie')
+        self.b = finData.load.Loader('BB Biotech', 'Aktie', 'A0NFN3', 'CH0038389992', 'BB-Biotech-Aktie')
 
     def tearDown(self):
         pass
 
-    def test_htmlTab2dict(self):
-        from bs4 import BeautifulSoup
-        htmlTable = (u'<table><tbody>' +
-                u'<tr><td>&nbsp;</td><td>2008</td><td>2009</td><td>2010</td><td>2011</td><td>2012</td></tr>' +
-                u'<tr><td>Personal am Jahresende</td><td>38.982</td><td>39.596</td><td>42.541</td><td>46.824</td><td>46.306</td></tr>' +
-                u'<tr><td>Gewinn je Mitarbeiter</td><td>16.469,14</td><td>6.187,49</td><td>13.328,32</td><td>13.091,58</td><td>11.359,22</td></tr>' +
-                u'<tr><td colspan="10"><div><h3>Personal</h3><div>blablabla</div></div></td></tr>' +
-                u'</tbody></table>')
-        soup = BeautifulSoup(htmlTable, 'lxml')
-        res = finData.load.htmlTab2dict(soup)
-        self.assertEqual(res['colnames'], ['2008', '2009', '2010', '2011', '2012'])
-        self.assertEqual(res['rownames'], ['Personal am Jahresende', 'Gewinn je Mitarbeiter'])
-        self.assertEqual(res['data'][0], ['38.982', '39.596', '42.541', '46.824', '46.306'])
-        with self.assertRaises(UserWarning):
-            finData.load.htmlTab2dict(soup, hasRownames=False, removeEmpty=False)
 
-    def test_FundamentalTables(self):
-        url_chr = 'https://www.boerse.de/fundamental-analyse/Adidas-Aktie/DE000A1EWWW0'
-        res = finData.load.FundamentalTables(url_chr, ids = ['guv'], texts = ['Marktkapitalisierung'])
+    def test_getFundamentalTables(self):
+        self.assertEqual(self.a.fund_url, 'https://www.boerse.de/fundamental-analyse/Adidas-Aktie/DE000A1EWWW0')
+        res = self.a.getFundamentalTables(True, ids = ['guv'], texts = ['Marktkapitalisierung'])
         self.assertEqual(len(res['marktk']['colnames']), 9)
         self.assertEqual(res['marktk']['rownames'], ['Anzahl der Aktien', 'Marktkapitalisierung'])
-        idx16 = [i for (i, y) in enumerate(res['marktk']['colnames']) if y == '2016']
-        self.assertEqual(map(lambda x: x[idx16[0]], res['marktk']['data']), ['201,49', '30.253,57'])
-        self.assertEqual(map(lambda x: x[idx16[0]], res['guv']['data']), ['19.291', '9.379', '1.491', '1.444', '1.017', '403,00'])
-        url_chr = 'https://www.boerse.de/fundamental-analyse/BB-Biotech-Aktie/CH0038389992'
-        res = finData.load.FundamentalTables(url_chr, ids = ['guv'], texts = [])
-        self.assertEqual(res, {'guv': {'colnames': [], 'data': [], 'rownames': []}})
+        idx16 = [i for (i, y) in enumerate(res['marktk']['colnames']) if y.decode() == '2016']
+        self.assertEqual(list(map(lambda x: x[idx16[0]], res['marktk']['data'])), ['201,49', '30.253,57'])
+        self.assertEqual(list(map(lambda x: x[idx16[0]], res['guv']['data'])), ['19.291', '9.379', '1.491', '1.444', '1.017', '403,00'])
+        self.assertEqual(self.b.fund_url, 'https://www.boerse.de/fundamental-analyse/BB-Biotech-Aktie/CH0038389992')
+        self.b.getFundamentalTables()
+        self.assertEqual(self.b.fund_table, self.b.getFundamentalTables(True))
+
 
     def test_DividendTable(self):
-        url_chr = 'https://www.boerse.de/dividenden/Adidas-Aktie/DE000A1EWWW0'
-        res = finData.load.DividendTable(url_chr)
+        self.assertEqual(self.a.divid_url, 'https://www.boerse.de/dividenden/Adidas-Aktie/DE000A1EWWW0')
+        res = self.a.getDividendTable(True)
         self.assertEqual(res['colnames'], ['Datum', 'Dividende', 'Ver\xc3\xa4nderung', 'Rendite'])
-        idx16 = [i for (i, d) in enumerate(map(lambda x: x[0], res['data'])) if d == '13.05.16']
+        idx16 = [i for (i, d) in enumerate(map(lambda x: x[0], res['data'])) if d.decode() == '13.05.16']
         self.assertEqual(res['data'][idx16[0]], ['13.05.16', '1,60', '6,67%', '1,07%'])
         with self.assertRaises(KeyError):
             res['rownames']
+        self.assertEqual(self.b.divid_url, 'https://www.boerse.de/dividenden/BB-Biotech-Aktie/CH0038389992')
+        self.b.getDividendTable()
+        self.assertEqual(self.b.divid_table, self.b.getDividendTable(True))
 
 
 class yahoo_api(unittest.TestCase):
