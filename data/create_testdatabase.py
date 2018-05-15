187e787db663574be2569845b3ab7809a043b50d
Marc Schwering <schweringmarc01@gmail.com>
2018-05-15 23:24:55 +0200

    testdb mit data auffüllen funktioniert
    muss noch die restlichen tabellen machen

--- data/create_testdatabase.py
+++ data/create_testdatabase.py
@@ -3,9 +3,15 @@ from data.schemata.schema_init import schema_init as schema  # the current schem
 from psycopg2.extensions import AsIs
 import psycopg2
 import argparse
-#TODO Zeilen einfügen für testdaten
-# args_str = ','.join(cur.mogrify("(%s,%s,%s,%s,%s,%s,%s,%s,%s)", x) for x in tup)
-# cur.execute("INSERT INTO table VALUES " + args_str)
+import pickle
+
+stock_cols = ("'{name}','{isin}','{wkn}','{typ}','{currency}',"
+              "'{boerse_name}','{avan_ticker}'")
+tab_cols = {
+    'guv': "{year},{umsatz},{bruttoergeb},{EBIT},{EBT},{jahresueber},{dividendena}",
+    'person': "{year},{personal},{aufwand},{umsatz},{gewinn}"
+}
+#TODO verbleibende tabellen definieren
 
 
 def connector(dbname, user, host, port, password=""):
@@ -15,11 +21,43 @@ def connector(dbname, user, host, port, password=""):
         return psycopg2.connect(dbname=dbname, user=user, host=host, port=port, password=password)
 
 
+def insertStatement(schema, table, cols, vals):
+    return "INSERT INTO {schema}.{table} ({cols}) VALUES {vals}" \
+        .format(schema=AsIs(schema), table=AsIs(table),
+                cols=cols, vals='({})'.format('),('.join(vals)))
+
+
+def inserTestdata(data, conn, schema_name):
+    # insert stock, DB increments stock id
+    cols = ','.join(['name', 'isin', 'wkn', 'typ',
+                     'currency', 'boerse_name', 'avan_ticker'])
+    vals = [stock_cols.format(**data[t]['stock']) for t in data]
+    with conn.cursor() as cur:
+        cur.execute(insertStatement(schema_name, 'stock', cols, vals))
+        cur.execute("SELECT id, avan_ticker FROM {schema}.stock".format(schema=schema_name))
+        stock_ids = cur.fetchall()
+
+    # for each stock get stock id and insert all tables
+    for stock in data:
+        stock_id = [tup[0] for tup in stock_ids if tup[1] == stock][0]
+        with conn.cursor() as cur:
+            for tab in tab_cols:
+                d = data[stock][tab].fillna('NULL')
+                cols = ','.join(['stock_id'] + d.columns.tolist())
+                vals = [tab_cols[tab].format(**r) for i, r in d.iterrows()]
+                vals = [','.join([str(stock_id), v]) for v in vals]
+                cur.execute(insertStatement(schema_name, tab, cols, vals))
+
+
 def create(db_name, schema_name, user, host, port, password):
-    print("Creating schema...")
     conn = connector(dbname=db_name, user=user, host=host, port=port, password=password)
-    schema(schema_name=schema_name, conn=conn)
-    print("Populating with some data...")
+    with conn:
+        print("Creating schema...")
+        schema(schema_name=schema_name, conn=conn)
+        print("Populating with some data...")
+        with open('data/testdata/testdata.pkl', 'rb') as inf:
+            data = pickle.load(inf)
+        inserTestdata(data, conn, schema_name)
 
 
 def drop(db_name, schema_name, user, host, port, password):
--- data/schemata/schema_init.py
+++ data/schemata/schema_init.py
@@ -4,131 +4,128 @@ from psycopg2.extensions import AsIs
 
 
 def schema_init(schema_name, conn):
-    with conn:
-        with conn.cursor() as cur:
-                cur.execute("""CREATE SCHEMA IF NOT EXISTS %(schema_name)s""",
-                            {'schema_name': AsIs(schema_name)})
+    with conn.cursor() as cur:
+            cur.execute("""CREATE SCHEMA IF NOT EXISTS %(schema_name)s""",
+                        {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.stock (
-                      id          INTEGER PRIMARY KEY,
-                      name        VARCHAR(50) NOT NULL,
-                      isin        VARCHAR(50) UNIQUE NOT NULL,
-                      wkn         VARCHAR(50) UNIQUE NOT NULL,
-                      typ         VARCHAR(10) NOT NULL,
-                      currency    VARCHAR(5) NOT NULL,
-                      boerse_name VARCHAR(50) UNIQUE NOT NULL,
-                      avan_ticker VARCHAR(50) UNIQUE NOT NULL
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.stock (
+                  id          SERIAL PRIMARY KEY,
+                  name        VARCHAR(50) NOT NULL,
+                  isin        VARCHAR(50) UNIQUE NOT NULL,
+                  wkn         VARCHAR(50) UNIQUE NOT NULL,
+                  typ         VARCHAR(10) NOT NULL,
+                  currency    VARCHAR(5) NOT NULL,
+                  boerse_name VARCHAR(50) UNIQUE NOT NULL,
+                  avan_ticker VARCHAR(50) UNIQUE NOT NULL
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.guv (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      umsatz      INTEGER,    -- in millionen EUR
-                      bruttoergeb INTEGER,    -- in millionen EUR
-                      EBIT        INTEGER,    -- in millionen EUR
-                      EBT         INTEGER,    -- in millionen EUR
-                      jahresueber INTEGER,    -- in millionen EUR
-                      dividendena INTEGER     -- in millionen EUR
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.guv (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  umsatz      INTEGER,    -- in millionen EUR
+                  bruttoergeb INTEGER,    -- in millionen EUR
+                  EBIT        INTEGER,    -- in millionen EUR
+                  EBT         INTEGER,    -- in millionen EUR
+                  jahresueber INTEGER,    -- in millionen EUR
+                  dividendena INTEGER     -- in millionen EUR
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.bilanz (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      umlaufvermo FLOAT,      -- in millionen EUR
-                      anlagevermo FLOAT,      -- in millionen EUR
-                      sum_aktiva  FLOAT,      -- in millionen EUR
-                      kurzfr_verb FLOAT,      -- in millionen EUR
-                      langfr_verb FLOAT,      -- in millionen EUR
-                      gesamt_verb FLOAT,      -- in millionen EUR
-                      eigenkapita FLOAT,      -- in millionen EUR
-                      sum_passiva FLOAT,      -- in millionen EUR
-                      eigen_quote DECIMAL(5, 2), -- in percent
-                      fremd_quote DECIMAL(5, 2)  -- in percent
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.bilanz (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  umlaufvermo FLOAT,      -- in millionen EUR
+                  anlagevermo FLOAT,      -- in millionen EUR
+                  sum_aktiva  FLOAT,      -- in millionen EUR
+                  kurzfr_verb FLOAT,      -- in millionen EUR
+                  langfr_verb FLOAT,      -- in millionen EUR
+                  gesamt_verb FLOAT,      -- in millionen EUR
+                  eigenkapita FLOAT,      -- in millionen EUR
+                  sum_passiva FLOAT,      -- in millionen EUR
+                  eigen_quote DECIMAL(5, 2), -- in percent
+                  fremd_quote DECIMAL(5, 2)  -- in percent
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.kennza (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      gewinn_verw DECIMAL(5,2),  -- je Aktie
-                      gewinn_unvw DECIMAL(5,2),  -- je Aktie
-                      umsatz      DECIMAL(5,2),  -- je Aktie
-                      buchwert    DECIMAL(5,2),  -- je Aktie
-                      dividende   DECIMAL(5,2),  -- je Aktie
-                      KGV         DECIMAL(5,2),  -- Kurs-Gewinn-Verh
-                      KBV         DECIMAL(5,2),  -- Kurs-Buchwert-Verh
-                      KUV         DECIMAL(5,2)   -- Kurs-Umsatz-Verh
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.kennza (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  gewinn_verw DECIMAL(5,2),  -- je Aktie
+                  gewinn_unvw DECIMAL(5,2),  -- je Aktie
+                  umsatz      DECIMAL(5,2),  -- je Aktie
+                  buchwert    DECIMAL(5,2),  -- je Aktie
+                  dividende   DECIMAL(5,2),  -- je Aktie
+                  KGV         DECIMAL(5,2),  -- Kurs-Gewinn-Verh
+                  KBV         DECIMAL(5,2),  -- Kurs-Buchwert-Verh
+                  KUV         DECIMAL(5,2)   -- Kurs-Umsatz-Verh
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.rentab (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      umsatzren   DECIMAL(5,2),  -- in percent
-                      eigenkapren DECIMAL(5,2),  -- in percent
-                      geskapren   DECIMAL(5,2),  -- in percent
-                      dividren    DECIMAL(5,2)   -- in percent
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.rentab (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  umsatzren   DECIMAL(5,2),  -- in percent
+                  eigenkapren DECIMAL(5,2),  -- in percent
+                  geskapren   DECIMAL(5,2),  -- in percent
+                  dividren    DECIMAL(5,2)   -- in percent
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.person (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      personal    INTEGER,       -- am Jahresende
-                      aufwand     FLOAT,         -- Personalaufwand in Mio
-                      umsatz      FLOAT,         -- je Mitarbeiter
-                      gewinn      FLOAT          -- je Mitarbeiter
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.person (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  personal    INTEGER,       -- am Jahresende
+                  aufwand     FLOAT,         -- Personalaufwand in Mio
+                  umsatz      FLOAT,         -- je Mitarbeiter
+                  gewinn      FLOAT          -- je Mitarbeiter
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.marktk (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      year        INTEGER NOT NULL,
-                      zahl_aktien INTEGER,
-                      marktkapita FLOAT   -- in Mio Euro
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.marktk (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  year        INTEGER NOT NULL,
+                  zahl_aktien INTEGER,
+                  marktkapita FLOAT   -- in Mio Euro
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.divid (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      datum       DATE NOT NULL,
-                      dividende   DECIMAL(5, 2),
-                      veraenderu  DECIMAL(5, 2),   -- in percent
-                      rendite     DECIMAL(5, 2)    -- in percent
-                    );
-                """, {'schema_name': AsIs(schema_name)})
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.divid (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  datum       DATE NOT NULL,
+                  dividende   DECIMAL(5, 2),
+                  veraenderu  DECIMAL(5, 2),   -- in percent
+                  rendite     DECIMAL(5, 2)    -- in percent
+                );
+            """, {'schema_name': AsIs(schema_name)})
 
-                cur.execute("""
-                    CREATE TABLE IF NOT EXISTS %(schema_name)s.hist (
-                      id          INTEGER PRIMARY KEY,
-                      stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
-                      datum       DATE NOT NULL,
-                      open        DECIMAL(7, 2),
-                      high        DECIMAL(7, 2),
-                      low         DECIMAL(7, 2),
-                      close       DECIMAL(7, 2),
-                      adj_close   DECIMAL(7, 2),
-                      volume      INTEGER,
-                      divid_amt   DECIMAL(5, 2),
-                      split_coef  DECIMAL(5, 2)
-                    );
-                """, {'schema_name': AsIs(schema_name)})
-
-    conn.close()
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS %(schema_name)s.hist (
+                  id          SERIAL PRIMARY KEY,
+                  stock_id    INTEGER REFERENCES %(schema_name)s.stock ( id ),
+                  datum       DATE NOT NULL,
+                  open        DECIMAL(7, 2),
+                  high        DECIMAL(7, 2),
+                  low         DECIMAL(7, 2),
+                  close       DECIMAL(7, 2),
+                  adj_close   DECIMAL(7, 2),
+                  volume      INTEGER,
+                  divid_amt   DECIMAL(5, 2),
+                  split_coef  DECIMAL(5, 2)
+                );
+            """, {'schema_name': AsIs(schema_name)})
