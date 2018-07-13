4eb3de755fe661d4079b61828ec3243a5fb80089
Marc Schwering <schweringmarc01@gmail.com>
2018-07-13 21:15:00 +0200

    schema fertig und tested

--- finData/schema.py
+++ finData/schema.py
@@ -1,51 +1,36 @@
 # This Python file uses the following encoding: utf-8
 import pandas as pd
 
-# TODO date types sollten eigentlich aus sql geladen werden
-# TODO yearly und daily könnten in table namen encoded werden
 # TODO constriants (zB currency) sollten in sql definiert sein
-
-# from finData.dbconnector import DBConnector
-# db = DBConnector('findata_test', 'postgres', '127.0.0.1', 5432)
-# schema = Schema('findata_init2', 'stock', db)
+# TODO adapter klasse für scraper -> schema
 
 
 class Schema(object):
-
-        # TODO wie krieg ich column names
-        # TODO wie krieg ich data types?
-        # TODO aus namen, update rate extrahieren
 
     def __init__(self, name, stock_table, db):
         self.name = name
         self.stock_table = stock_table
         self._db = db
         self.tables = self._getTables()
-        if self.stock_table not in self.tables:
-            raise AttributeError('Stock table %s not defined in tables' % self.stock_table)
         self._info_schema = self._getInfoSchema()
-        #convs = [tab for tab in self.conversions]
-        # if set(tabs) != set(convs):
-        #     raise AttributeError('Definitions for tables and conversions do not match')
+        if self.stock_table not in self.tables:
+            raise AttributeError(
+                'Stock table %s not defined in tables' % self.stock_table)
+        info_tabs = self._info_schema.get('table_name').tolist()
+        if set(self.tables) != set(info_tabs):
+            raise AttributeError(
+                ('Weird error occured. DB might be fucked up.\n'
+                 'DB has these tables: {tabs}\n'
+                 'But information schema has these: {infos}')
+                .format(tabs=self.tables, infos=info_tabs))
 
     def table(self, name):
         if name not in self.tables:
             raise ValueError('table %s not defined in schema %s' % (name, self.name))
-        # if name == self.stock_table:
-        #     update_rate = None
-        #     cols = [key for key in self.tables.get(name)]
-        #     col_types = {key: 'str' for key in self.tables.get(name)}
-        #     col_types['id'] = 'int'
-        #     is_stock_table = True
-        # else:
-        #     update_rate = 'daily' if name in self.daily_updates else 'yearly'
-        #     cols = [key for key in self.tables.get(name)]
-        #     col_types = {key: self.getType(key) for key in self.tables.get(name)}
-        #     is_stock_table = False
-        return Table(name, self._db, name == self.stock_table, self.name)
-
-    def listTables(self):
-        return [tab for tab in self.tables]
+        df = self._info_schema
+        df = df.loc[df['table_name'] == name]
+        df = df.sort_values(by=['ordinal_position'])
+        return Table(name, name == self.stock_table, df, self.name)
 
     def _getTables(self):
         query = ("""SELECT tablename FROM pg_catalog.pg_tables """
@@ -59,50 +44,42 @@ class Schema(object):
         query = ("""SELECT {cols} FROM information_schema.columns """
                  """WHERE table_schema = %(schema)s""").format(cols=', '.join(info_cols))
         args = {'schema': self.name}
-        df = self._db.query(query, args, fetch='all')
+        res = self._db.query(query, args, fetch='all')
         return pd.DataFrame(res, columns=info_cols)
-
-    @classmethod
-    def getType(cls, col_name):
-        if col_name in cls.date_columns:
-            return 'date'
-        if col_name in cls.int_columns:
-            return 'int'
-        return 'numeric'
 
 
 class Table(object):
 
     id_column = 'id'
+    time_columns = ['datum', 'jahr']
 
-    def __init__(self, name, db, stock, schema):
+    def __init__(self, name, stock, info_table, schema):
         self.name = name
-        self._isStock = stock
-        self._db = db
         self._schema = schema
-        self.columns = self._getColumns()
+        self._isStock = stock
+        self._info_table = info_table
         self.update_rate = self._getUpdateRate()
-        # self.update_rate = update_rate
-        # self.columns = columns
-        # self._column_types = column_types
+        self.columns = self._getColumns()
+        self.time_column = self._getTimeColumn()
         self.insert_statement = self._getInsertStatement()
 
     def column(self, name):
-        if name not in [col for col in self._column_types]:
-            raise ValueError('column %s not defined in table %s' %
-                             (name, self.name))
-        return Column(name, self._column_types.get(name))
+        if name not in self.columns:
+            raise ValueError(
+                'column %s not defined in table %s' % (name, self.name))
+        df = self._info_table
+        return Column(name, df.loc[df['column_name'] == name])
 
     def _getColumns(self):
-        query = ("""SELECT column_name FROM information_schema.columns """
-                 """WHERE table_schema = %(schema)s """
-                 """AND table_name = %(table)s""")
-        args = {'schema': self._schema, 'table': self.name}
-        res = self._db.query(query, args, fetch='all')
-        return [col[0] for col in res]
+        return self._info_table.get('column_name').tolist()
 
     def _getUpdateRate(self):
-        return self.name.split('_')[-1]
+        if self._isStock:
+            return None
+        splitted = self.name.split('_')
+        if len(splitted) < 2:
+            return None
+        return splitted[-1]
 
     def _getInsertStatement(self):
         columns = [col for col in self.columns if col != self.id_column]
@@ -112,9 +89,28 @@ class Table(object):
         return """INSERT INTO {loc} ({cols}) VALUES ({vals})""" \
                .format(cols=cols, vals=vals, loc=loc)
 
+    def _getTimeColumn(self):
+        if self._isStock:
+            return None
+        cols = [col for col in self.columns if col in self.time_columns]
+        return cols[0]
+
 
 class Column(object):
 
-    def __init__(self, col_name, col_type):
+    def __init__(self, col_name, info_col):
         self.name = col_name
-        self.type = col_type
+        self.type = self._getType(info_col)
+
+    def _getType(self, df):
+        data_type = df.get('data_type').tolist()[0]
+        if data_type in ['integer']:
+            return 'integer'
+        if data_type in ['double precision', 'numeric']:
+            return 'numeric'
+        if data_type in ['character varying']:
+            return 'string'
+        if data_type in ['date']:
+            return 'date'
+        raise AttributeError('data type {d} of column {c} unknown'
+                             .format(d=data_type, c=self.name))
--- finData/test_schema.py
+++ finData/test_schema.py
@@ -17,10 +17,15 @@ DB = MagicMock()
 DB.query = MagicMock(return_value=[(tab,) for tab in TABLES])
 
 
-# patching DB connection
 def patchInfoSchema(args_list):
     with patch('finData.schema.Schema._getInfoSchema') as info:
         info.return_value = INFO_SCHEMA
+        return Schema(*args_list)
+
+
+def patchGetTables(args_list):
+    with patch('finData.schema.Schema._getTables') as tables:
+        tables.return_value = TABLES
         return Schema(*args_list)
 
 
@@ -36,10 +41,12 @@ class catchStdout:
         sys.stdout = sys.__stdout__
 
 
-class SchemaInit(unittest.TestCase):
+class InfoSchemaPatched(unittest.TestCase):
 
     def setUp(self):
-        self.S = patchInfoSchema(['schema_name', 'stock', DB])
+        db = MagicMock()
+        db.query = MagicMock(return_value=[(tab,) for tab in TABLES])
+        self.S = patchInfoSchema(['schema_name', 'stock', db])
 
     def test_schemaArgs(self):
         self.assertEqual(self.S.name, 'schema_name')
@@ -61,182 +68,205 @@ class SchemaInit(unittest.TestCase):
         self.assertEqual(calls[0][1][0], exp)
         self.assertEqual(calls[0][1][1], {'schema_name': 'schema_name'})
 
-# class SchemaInit(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.db = MagicMock()
-#         self.db.query = MagicMock(return_value=[('stock',), ('divid_yearly',)])
-#
-#     def test_normalInit(self):
-#         S = Schema('schema_name', 'stock', self.db)
-#         self.assertEqual(S.name, 'schema_name')
-#         self.assertEqual(S.stock_table, 'stock')
-#         self.assertEqual(S.tables, ['stock', 'divid_yearly'])
-#
-#     def test_wrongStockTable(self):
-#         with self.assertRaises(AttributeError):
-#             Schema('schema_name', 'stck', self.db)
-#
-#     def test_correctCalls(self):
-#         S = Schema('schema_name', 'stock', self.db)
-#         calls = S._db.method_calls
-#         self.assertEqual(len(calls), 1)
-#         exp = ("""SELECT tablename FROM pg_catalog.pg_tables """
-#                """WHERE schemaname = %(schema_name)s""")
-#         self.assertEqual(calls[0][1][0], exp)
-#         self.assertEqual(calls[0][1][1], {'schema_name': 'schema_name'})
-#
-#
-# class CreatingTable(unittest.TestCase):
-#
-#     def setUp(self):
-#         db = MagicMock()
-#         db.query = MagicMock(return_value=[('stock',), ('divid_yearly',)])
-#         self.S = Schema('schema_name', 'stock', db)
-#
-#     def test_normalTable(self):
-#         T = self.S.table('divid_yearly')
-#         self.assertEqual(T.name, 'divid_yearly')
-#         self.assertEqual(type(T).__name__, 'Table')
-#         self.assertFalse(T._isStock)
-#
-#     def test_wrongTable(self):
-#         with self.assertRaises(ValueError):
-#             self.S.table('divid')
-#
-#     def test_stockTable(self):
-#         T = self.S.table('stock')
-#         self.assertTrue(T._isStock)
-#
-#
-# class TableAttr(unittest.TestCase):
-#
-#     def setUp(self):
-#         db = MagicMock()
-#         db.query = MagicMock(return_value=[('stock',), ('divid_yearly',)])
-#         S = Schema('schema_name', 'stock', db)
-#         db.query = MagicMock(return_value=[('year',), ('divid',)])
-#         self.T = S.table('divid_yearly')
-#
-#     def test_correctColumns(self):
-#         self.assertEqual(self.T.columns, ['year', 'divid'])
-#
-#     def test_updateRate(self):
-#         self.assertEqual(self.T.update_rate, 'yearly')
-#
-#     def test_insertStatement(self):
-#         exp = ("""INSERT INTO schema_name.divid_yearly (year,divid) """
-#                """VALUES (%(year)s,%(divid)s)""")
-#         self.assertEqual(self.T.insert_statement, exp)
+
+class GetTablesPatched(unittest.TestCase):
+
+    def setUp(self):
+        db = MagicMock()
+        db.query = MagicMock(return_value=INFO_SCHEMA.to_dict())
+        self.S = patchGetTables(['schema_name', 'stock', db])
+
+    def test_schemaArgs(self):
+        self.assertEqual(self.S.name, 'schema_name')
+        self.assertEqual(self.S.stock_table, 'stock')
+        self.assertEqual(self.S.tables, TABLES)
+
+    def test_expectedInfoSchema(self):
+        self.assertEqual(self.S._info_schema.shape, (17, 4))
+        ords = self.S._info_schema['ordinal_position'].tolist()
+        exp = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 4]
+        self.assertEqual(ords, exp)
+
+    def test_correctCalls(self):
+        # _getTables was patched so only getInfoSchema call
+        calls = self.S._db.method_calls
+        self.assertEqual(len(calls), 1)
+        info_cols = ['table_name', 'column_name', 'ordinal_position', 'data_type']
+        exp = ("""SELECT {cols} FROM information_schema.columns """
+               """WHERE table_schema = %(schema)s""").format(cols=', '.join(info_cols))
+        self.assertEqual(calls[0][1][0], exp)
+        self.assertEqual(calls[0][1][1], {'schema': 'schema_name'})
 
 
-# class SchemaBehaviour(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.S = GoodSchema()
-#
-#     def test_nameAttr(self):
-#         self.assertEqual(self.S.name, 'findata_init')
-#
-#     def test_listTables(self):
-#         self.assertSetEqual(set(self.S.listTables()), set(['stock', 'marktk', 'hist']))
-#
-#     def test_getWrongTable(self):
-#         with self.assertRaises(ValueError):
-#             self.S.table('asd')
-#
-#     def test_getTable(self):
-#         tab = self.S.table('hist')
-#         self.assertEqual(type(tab).__name__, 'Table')
-#
-#     def test_tablesAndConversionsNotMatching(self):
-#         with self.assertRaises(AttributeError):
-#             self.S = WrongSchema()
-#
-#     def test_missingStockTable(self):
-#         with self.assertRaises(AttributeError):
-#             self.S = MissingStockSchema()
-#
-#
-# class TableBehaviour(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.T = GoodSchema().table('hist')
-#
-#     def test_nameAttr(self):
-#         self.assertEqual(self.T.name, 'hist')
-#
-#     def test_columnsAttr(self):
-#         self.assertEqual(self.T.columns, ['stock_id', 'datum', 'open'])
-#
-#     def test_updateRate(self):
-#         self.assertEqual(self.T.update_rate, 'daily')
-#
-#     def test_insertStatement(self):
-#         exp = ("""INSERT INTO %(schema_name)s.%(table_name)s """
-#                """(stock_id,datum,open) VALUES """
-#                """(%(stock_id)s,%(datum)s,%(open)s)""")
-#         self.assertEqual(self.T.insert_statement, exp)
-#
-#     def test_getWrongColumn(self):
-#         with self.assertRaises(ValueError):
-#             self.T.column('asd')
-#
-#     def test_getColumn(self):
-#         col = self.T.column('stock_id')
-#         self.assertEqual(type(col).__name__, 'Column')
-#
-#
-# class StockTableBehaviour(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.T = GoodSchema().table('stock')
-#
-#     def test_nameAttr(self):
-#         self.assertEqual(self.T.name, 'stock')
-#
-#     def test_columnsAttr(self):
-#         self.assertEqual(self.T.columns, ['id', 'isin'])
-#
-#     def test_updateRate(self):
-#         self.assertIsNone(self.T.update_rate)
-#
-#     def test_insertStatement(self):
-#         exp = ("""INSERT INTO %(schema_name)s.%(table_name)s """
-#                """(isin) VALUES (%(isin)s)""")
-#         self.assertEqual(self.T.insert_statement, exp)
-#
-#     def test_getWrongColumn(self):
-#         with self.assertRaises(ValueError):
-#             self.T.column('asd')
-#
-#     def test_getColumn(self):
-#         col = self.T.column('id')
-#         self.assertEqual(type(col).__name__, 'Column')
-#
-#
-# class ColumnBehaviour(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.C = GoodSchema().table('hist').column('datum')
-#
-#     def test_nameAttr(self):
-#         self.assertEqual(self.C.name, 'datum')
-#
-#     def test_typeAttr(self):
-#         self.assertEqual(self.C.type, 'date')
-#
-#
-# class StockColumnBehaviour(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.C = GoodSchema().table('stock').column('isin')
-#         self.C2 = GoodSchema().table('stock').column('id')
-#
-#     def test_nameAttr(self):
-#         self.assertEqual(self.C.name, 'isin')
-#         self.assertEqual(self.C2.name, 'id')
-#
-#     def test_typeAttr(self):
-#         self.assertEqual(self.C.type, 'str')
-#         self.assertEqual(self.C2.type, 'int')
+class SchemaInitErrors(unittest.TestCase):
+
+    def setUp(self):
+        self.db = MagicMock()
+        self.db.query = MagicMock(return_value=[(tab,) for tab in TABLES[:3]])
+
+    def test_wrongStockTable(self):
+        with self.assertRaises(AttributeError):
+            self.S = patchInfoSchema(['schema_name', 'asd', self.db])
+
+    def test_fuckedUpSchema(self):
+        with self.assertRaises(AttributeError):
+            self.S = patchInfoSchema(['schema_name', 'stock', self.db])
+
+
+class InitTable(unittest.TestCase):
+
+    def setUp(self):
+        self.S = patchInfoSchema(['schema_name', 'stock', DB])
+
+    def test_wrongTable(self):
+        with self.assertRaises(ValueError):
+            self.S.table('asd')
+
+    def test_stockTable(self):
+        self.T = self.S.table('stock')
+        self.assertEqual(self.T.name, 'stock')
+        self.assertTrue(self.T._isStock)
+
+    def test_normalTable(self):
+        self.T = self.S.table('divid_yearly')
+        self.assertEqual(self.T.name, 'divid_yearly')
+        self.assertFalse(self.T._isStock)
+
+
+class StockTable(unittest.TestCase):
+
+    cols = ['id', 'name', 'isin']
+
+    def setUp(self):
+        self.T = patchInfoSchema(['schema_name', 'stock', DB]).table('stock')
+
+    def test_updateRate(self):
+        self.assertIsNone(self.T.update_rate)
+
+    def test_infoTable(self):
+        act = self.T._info_table['ordinal_position'].tolist()
+        self.assertEqual(act, [1, 2, 3])
+
+    def test_infoTable(self):
+        self.assertEqual(self.T.columns, self.cols)
+
+    def test_infoTable(self):
+        exp = 'INSERT INTO schema_name.stock ({a}) VALUES (%({b})s)' \
+              .format(a=','.join(self.cols[1:]), b=')s,%('.join(self.cols[1:]))
+        self.assertEqual(self.T.insert_statement, exp)
+
+    def test_timepointColumn(self):
+        self.assertIsNone(self.T.time_column)
+
+    def test_allColumnTypes(self):
+        exp = ['integer', 'string', 'string']
+        act = [self.T.column(col).type for col in self.T.columns]
+        self.assertEqual(exp, act)
+
+
+class DividTable(unittest.TestCase):
+
+    cols = ['id', 'stock_id', 'datum', 'dividende']
+
+    def setUp(self):
+        self.T = patchInfoSchema(['schema_name', 'stock', DB]).table('divid_yearly')
+
+    def test_updateRate(self):
+        self.assertEqual(self.T.update_rate, 'yearly')
+
+    def test_infoTable(self):
+        act = self.T._info_table['ordinal_position'].tolist()
+        self.assertEqual(act, [1, 2, 3, 4])
+
+    def test_infoTable(self):
+        self.assertEqual(self.T.columns, self.cols)
+
+    def test_infoTable(self):
+        exp = 'INSERT INTO schema_name.divid_yearly ({a}) VALUES (%({b})s)' \
+              .format(a=','.join(self.cols[1:]), b=')s,%('.join(self.cols[1:]))
+        self.assertEqual(self.T.insert_statement, exp)
+
+    def test_timepointColumn(self):
+        self.assertEqual(self.T.time_column, 'datum')
+
+    def test_allColumnTypes(self):
+        exp = ['integer', 'integer', 'date', 'numeric']
+        act = [self.T.column(col).type for col in self.T.columns]
+        self.assertEqual(exp, act)
+
+
+class HistTable(unittest.TestCase):
+
+    cols = ['id', 'stock_id', 'datum', 'open']
+
+    def setUp(self):
+        self.T = patchInfoSchema(['schema_name', 'stock', DB]).table('hist_daily')
+
+    def test_updateRate(self):
+        self.assertEqual(self.T.update_rate, 'daily')
+
+    def test_infoTable(self):
+        act = self.T._info_table['ordinal_position'].tolist()
+        self.assertEqual(act, [1, 2, 3, 4])
+
+    def test_infoTable(self):
+        self.assertEqual(self.T.columns, self.cols)
+
+    def test_infoTable(self):
+        exp = 'INSERT INTO schema_name.hist_daily ({a}) VALUES (%({b})s)' \
+              .format(a=','.join(self.cols[1:]), b=')s,%('.join(self.cols[1:]))
+        self.assertEqual(self.T.insert_statement, exp)
+
+    def test_timepointColumn(self):
+        self.assertEqual(self.T.time_column, 'datum')
+
+    def test_allColumnTypes(self):
+        exp = ['integer', 'integer', 'date', 'numeric']
+        act = [self.T.column(col).type for col in self.T.columns]
+        self.assertEqual(exp, act)
+
+
+class FundTable(unittest.TestCase):
+
+    cols = ['id', 'stock_id', 'jahr', 'umsatz', 'sum_aktiva', 'dividende']
+
+    def setUp(self):
+        self.T = patchInfoSchema(['schema_name', 'stock', DB]).table('fundamental_yearly')
+
+    def test_updateRate(self):
+        self.assertEqual(self.T.update_rate, 'yearly')
+
+    def test_infoTable(self):
+        act = self.T._info_table['ordinal_position'].tolist()
+        self.assertEqual(act, [1, 2, 3, 4, 5, 6])
+
+    def test_infoTable(self):
+        self.assertEqual(self.T.columns, self.cols)
+
+    def test_infoTable(self):
+        exp = 'INSERT INTO schema_name.fundamental_yearly ({a}) VALUES (%({b})s)' \
+              .format(a=','.join(self.cols[1:]), b=')s,%('.join(self.cols[1:]))
+        self.assertEqual(self.T.insert_statement, exp)
+
+    def test_timepointColumn(self):
+        self.assertEqual(self.T.time_column, 'jahr')
+
+    def test_allColumnTypes(self):
+        exp = ['integer', 'integer', 'integer', 'integer', 'numeric', 'numeric']
+        act = [self.T.column(col).type for col in self.T.columns]
+        self.assertEqual(exp, act)
+
+
+class ColumnInit(unittest.TestCase):
+
+    def setUp(self):
+        self.T = patchInfoSchema(['schema_name', 'stock', DB]).table('stock')
+
+    def test_wrongColumn(self):
+        with self.assertRaises(ValueError):
+            self.T.column('asd')
+
+    def test_unknownDataType(self):
+        self.T._info_table.iat[1, 3] = 'asd'
+        with self.assertRaises(AttributeError):
+            self.T.column('name')
