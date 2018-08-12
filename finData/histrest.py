3862ca436e1122018ddd691dcd1a7451b335c41c
Marc Schwering <schweringmarc01@gmail.com>
2018-08-12 22:46:19 +0200

    renames

--- finData/histrest.py
+++ finData/histrest.py
@@ -1,5 +1,5 @@
 # This Python file uses the following encoding: utf-8
-from finData.alphavantageAPI import AlphavantagAPI
+from finData.alphavantageapi import AlphavantagAPI
 import pandas as pd
 
 
